12c78729c7688d1a1e86a2fbacf58fc0600e11a2
mikey <mikey@hey.com>
2023-08-29 21:58:52 -0500

    fix indentation, add shuffle

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -1,36 +1,37 @@
 # import the json module from python3
 import json
+import random
 
 def read_file(file_name):
-  ''' read_file() opens file_name, a .json file, reads as json
-      and returns that data as a dictionary to make flashcards'''
-  with open(file_name, 'r') as f:
-   # debug
-   # print(data)
-   data = json.load(f)
-   return data
+    ''' read_file() opens file_name, a .json file, reads as json
+        and returns that data as a dictionary to make flashcards'''
+    with open(file_name, 'r') as f:
+        # debug
+        # print(data)
+        data = json.load(f)
+        return data
   
 def play_game(deck):
-   ''' play_game() runs through a round of flashcard reviews.
-   takes one argument, deck, which is the deck dictionary
-   returned from loading data with the read_file() function '''
+    ''' play_game() runs through a round of flashcard reviews.
+    takes one argument, deck, which is the deck dictionary
+    returned from loading data with the read_file() function '''
    
-   # initialize total as the length of the cards array
-   total = len(deck["cards"])  
-  
-   # initialize score as 0
-   score = 0
-   for i in deck["cards"]:
-       guess = input(i["q"] + " > ")
-       if guess.lower() == i["a"].lower():
-           # increment score up one
-           score += 1
-           # interpolate score and total into the response
-           print(f"Correct! Current score: {score}/{total}")
-       else:
-           print("Incorrect! The correct answer was", i["a"])
-           print(f"Current score: {score}/{total}")
-   print(f"\nThanks for studying! \nFinal Score {score}/{total}")
+    # initialize total as the length of the cards array
+    total = len(deck["cards"])  
+   
+     # initialize score as 0
+    score = 0
+    for i in deck["cards"]:
+        guess = input(i["q"] + " > ")
+        if guess.lower() == i["a"].lower():
+            # increment score up one
+            score += 1
+            # interpolate score and total into the response
+            print(f"Correct! Current score: {score}/{total}")
+        else:
+            print("Incorrect! The correct answer was", i["a"])
+            print(f"Current score: {score}/{total}")
+    print(f"\nThanks for studying! \nFinal Score {score}/{total}")
 
 # load data
 data = read_file(file_name='me-capitals.json')
@@ -39,19 +40,22 @@ data = read_file(file_name='me-capitals.json')
 player = True
 while player == True:
    
-   # play game
-   play_game(deck=data)
+  # shuffle deck
+    random.shuffle(data["cards"])
    
-   # get user input to continue or break
-   play_again = input("Would you like to play again? (yes/no) > ")
-   
-   if play_again == 'yes':
-      player = True
-   else:
-      break
+    # play game
+    play_game(deck=data)
+    
+    # get user input to continue or break
+    play_again = input("Would you like to play again? (yes/no) > ")
+    
+    if play_again == 'yes':
+       player = True
+    else:
+       break
 
 ''' Brainstorming New Features
-[] Ask them if they want to play again at the end.
+[X] Ask them if they want to play again at the end.
 [] Randomize the order of questions.
 [] Keep playing until the player gets all the questions right at least once.
 [] Keep playing until the player answers correctly 10 in a row.
