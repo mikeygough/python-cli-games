21252d4ce2fe28341399594c7faa3044aebb4612
mikey <mikey@hey.com>
2023-08-29 22:19:09 -0500

    add ability to choose different decks

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -1,20 +1,22 @@
 # import the json module from python3
-import json
-import random
+import json # to read json
+import random # to shuffle
+import os # to list available decks
 
 def read_file(file_name):
     ''' read_file() opens file_name, a .json file, reads as json
-        and returns that data as a dictionary to make flashcards'''
+        and returns that data as a dictionary to make flashcards. '''
     with open(file_name, 'r') as f:
         # debug
         # print(data)
         data = json.load(f)
         return data
   
+  
 def play_game(deck):
     ''' play_game() runs through a round of flashcard reviews.
     takes one argument, deck, which is the deck dictionary
-    returned from loading data with the read_file() function '''
+    returned from loading data with the read_file() function. '''
    
     # initialize total as the length of the cards array
     total = len(deck["cards"])  
@@ -35,8 +37,28 @@ def play_game(deck):
     
     return score, total
 
+
+def list_decks():
+    ''' list_decks() returns a list of strings with the available decks in ./decks '''
+    decks = []
+    for deck in os.listdir('./decks'):
+        decks.append(deck[:-5])
+    return decks
+
+decks = list_decks()
+
+print("Available Decks:")
+for deck in decks:
+    print(deck)
+    
+deck_choice = input("Please choose a deck to study: ")
+
+while deck_choice not in decks:
+    print("Invalid Deck! Please try again.")
+    deck_choice = input("Please choose a deck to study: ")
+
 # load data
-data = read_file(file_name='me-capitals.json')
+data = read_file(file_name=f"decks/{deck_choice}.json")
 
 # start study loop
 player = True
@@ -66,10 +88,10 @@ while player == True:
 ''' Brainstorming New Features
 [X] Ask them if they want to play again at the end.
 [X] Randomize the order of questions.
-[] Keep playing until the player gets all the questions right at least once.
-[] Keep playing until the player answers correctly 10 in a row.
+[X] Keep playing until the player gets all the questions right at least once.
 [] Create various data files that are different sets of questions and let people pick which one they want to do.
 [] Let people enter their own cards and save those as libraries of questions and answers.
+[] Keep playing until the player answers correctly 10 in a row.
 
 f-strings - Can you use f-strings to change how we do string interpolations throughout this program?
 
