3d1fd27d2904453aa2f8c9533787ec0e10700a25
mikey <mikey@hey.com>
2023-08-29 21:31:16 -0500

    add functions

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -1,40 +1,62 @@
 # import the json module from python3
 import json
 
-# open the file and parse the JSON
-with open('me-capitals.json', 'r') as f:
-    data = json.load(f)
-    # debug
-    # print(data)
-# note, the only difference between JSON and a python dictionary is the keys have '' 
-# single quotes whereas the JSON has "" double-quotes.
+def read_file(file_name):
+  ''' read_file() opens file_name, a .json file, reads as json
+      and returns that data as a dictionary to make flashcards'''
+  with open(file_name, 'r') as f:
+   # debug
+   # print(data)
+   data = json.load(f)
+   return data
+  
+def play_game(deck):
+   ''' play_game() runs through a round of flashcard reviews.
+   takes one argument, deck, which is the deck dictionary
+   returned from loading data with the read_file() function '''
+   
+   # initialize total as the length of the cards array
+   total = len(deck["cards"])  
+  
+   # initialize score as 0
+   score = 0
+   for i in deck["cards"]:
+       guess = input(i["q"] + " > ")
+       if guess.lower() == i["a"].lower():
+           # increment score up one
+           score += 1
+           # interpolate score and total into the response
+           print(f"Correct! Current score: {score}/{total}")
+       else:
+           print("Incorrect! The correct answer was", i["a"])
+           print(f"Current score: {score}/{total}")
+   print(f"\nThanks for studying! \nFinal Score {score}/{total}")
 
-# initialize total as the length of the cards array
-total = len(data["cards"])
-# initialize score as 0
-score = 0
+# load data
+data = read_file(file_name='me-capitals.json')
 
-for i in data["cards"]:
-    guess = input(i["q"] + " > ")
-
-    if guess.lower() == i["a"].lower():
-        # increment score up one
-        score += 1
-        # interpolate score and total into the response
-        print(f"Correct! Current score: {score}/{total}")
-    else:
-        print("Incorrect! The correct answer was", i["a"])
-        print(f"Current score: {score}/{total}")
-
-print(f"\nThanks for studying! \nFinal Score {score}/{total}")
+# start study loop
+player = True
+while player == True:
+   
+   # play game
+   play_game(deck=data)
+   
+   # get user input to continue or break
+   play_again = input("Would you like to play again? (yes/no) > ")
+   
+   if play_again == 'yes':
+      player = True
+   else:
+      break
 
 ''' Brainstorming New Features
-Ask them if they want to play again at the end.
-Randomize the order of questions.
-Keep playing until the player gets all the questions right at least once.
-Keep playing until the player answers correctly 10 in a row.
-Create various data files that are different sets of questions and let people pick which one they want to do.
-Let people enter their own cards and save those as libraries of questions and answers.
+[] Ask them if they want to play again at the end.
+[] Randomize the order of questions.
+[] Keep playing until the player gets all the questions right at least once.
+[] Keep playing until the player answers correctly 10 in a row.
+[] Create various data files that are different sets of questions and let people pick which one they want to do.
+[] Let people enter their own cards and save those as libraries of questions and answers.
 
 f-strings - Can you use f-strings to change how we do string interpolations throughout this program?
 
